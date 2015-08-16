55e73cb3b4e3dcdf402e3918cc0c878170fe686d
babakkeyvani <babak.keyvani@cpcc.edu>
2015-08-16 02:21:53 -0400

    added mock layout and clickHandler

--- project0/MyApplication/app/src/main/java/com/keyvani/android/myappportfolio/MainActivity.java
+++ project0/MyApplication/app/src/main/java/com/keyvani/android/myappportfolio/MainActivity.java
@@ -2,8 +2,10 @@ package com.keyvani.android.myappportfolio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,5 +36,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void displayToast(View view) {
+        Log.d("MainActivity", view.getResources().getResourceEntryName(view.getId()) + " Clicked!");
     }
 }
